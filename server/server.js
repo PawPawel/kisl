abc2699ec7f0798a9107b49849fe95e3399ee77f
unknown <stefanczyk.pawel.karol@gmail.com>
2019-12-01 22:46:09 +0100

    wysylanie maila z tokenem zrobione, brakuje zapisywania tokena w AD

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var crypto = require('crypto');
 
 const app = express();
+const nodemailer = require('nodemailer');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
@@ -66,15 +67,63 @@ app.post('/api/auth', (req, res) => {
 });
 
 app.post('/api/findEmail', (req, res) => {
-  console.log(req.body);
-  ad.find(req.body.email, function(err, user) {
-    console.log(user.user);
-    if (err) {
-      console.log('ERROR: ' +JSON.stringify(err));
-      return;
+  var query = 'cn=*';
+  var foundUser;    
+  ad.findUsers(query, function(err, users) {
+	  if (err) {
+		console.log('ERROR: ' +JSON.stringify(err));
+		return;
+    }    
+   users.forEach(element => {
+    if(element.mail){           
+      if(element.mail === req.body.email){
+        foundUser= element;   
+        
+      }
+    }
+   });     
+   if(!foundUser){
+   res.json('email not in db');
+   return;
+  }
+   else {        
+        const token = crypto.randomBytes(20).toString('hex');        
+        var retUser = {
+          user: foundUser,
+          token: token,
+          tokenExpires: Date.now()+360000
+        }
+        console.log("dane");
+        console.log(retUser.user);
+        console.log(retUser.token);
+        console.log(retUser.tokenExpires);
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'askprojekt0@gmail.com',
+          pass: 'kotki123!',
+        },
+      });
+      const mailOptions = {
+        from: 'ask@gmail.com',
+        to: `${retUser.user.mail}`,
+        subject: 'Link To Reset Password',
+        text:
+          'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
+          + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
+          + `http://localhost:3031/reset/${retUser.token}\n\n`
+          + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+      };
+      transporter.sendMail(mailOptions, (err, response) => {
+        if (err) {
+          console.error('there was an error sending email: ', err);
+        } else {
+          console.log('here is the res: ', response);
+          res.status(200).json('recovery email sent');
+        }
+      });
     }
   });
-  
 });
 
 const port = 5000;
